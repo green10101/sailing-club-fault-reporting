2de431bcdca6b190af2d3ffe5173f7d442c07b6d
Mark Green <mark@greengiants.org.uk>
2026-01-26 22:57:00 +0000

    Fix reset password view - remove username reference

--- src/Views/admin/reset_password.php
+++ src/Views/admin/reset_password.php
@@ -9,7 +9,7 @@
 <body>
     <div class="container">
         <?php include '../src/Views/layouts/nav.php'; ?>
-        <h1>Reset Password for <?php echo htmlspecialchars($user['username']); ?></h1>
+        <h1>Reset Password for <?php echo htmlspecialchars($user['name']); ?></h1>
 
         <?php if (isset($error)): ?>
             <div class="alert alert-danger"><?php echo htmlspecialchars($error); ?></div>
@@ -17,7 +17,7 @@
 
         <div class="card">
             <div class="card-body">
-                <form action="/admin/user/reset-password/<?php echo $user['id']; ?>" method="post">
+                <form action="index.php?route=/admin/user/reset-password/<?php echo $user['id']; ?>" method="post">
                     <div class="mb-3">
                         <label for="new_password" class="form-label">New Password</label>
                         <input type="text" class="form-control" id="new_password" name="new_password" required autofocus>
